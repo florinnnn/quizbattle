6c330ade1a8b5836ff0343f48542a6981d2a7378
Eoczi <106028382+Eoczi@users.noreply.github.com>
2024-04-27 19:29:30 +0300

    rares bunea

---
+++ Assets/Scripts/CustomDrag.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public interface ICustomDrag
+{
+    void OnCurrentDrag();
+}
+
+public class CustomDrag : MonoBehaviour, IDragHandler
+{
+    [SerializeField] GameObject objectToInteractWith;
+    private ICustomDrag onDrag;
+
+    void Start()
+    {
+        onDrag = objectToInteractWith.GetComponent<ICustomDrag>();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        onDrag.OnCurrentDrag();
+    }
+
+}
