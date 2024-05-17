34ce8260ccfe5dc435ace1a04d34f8d556dc3362
Bunea-Rares <rares.bunea02@e-uvt.ro>
2024-05-17 19:58:38 +0300

    unde-i ursul

---
+++ Assets/Scripts/WallHealth.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallHealth : MonoBehaviour
+{
+    public float maxHealth = 100f;
+    public float currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(float amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth < 0)
+        {
+            currentHealth = 0;
+        }
+        if(currentHealth == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
