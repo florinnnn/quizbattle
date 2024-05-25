a48156d80eca7169360d92371c1deff8f88e1eac
Alexaru <alex.bara09@yahoo.com>
2024-05-25 14:21:55 +0300

    adaugare kills

--- Assets/Scripts/Counter.cs
+++ Assets/Scripts/Counter.cs
@@ -4,14 +4,17 @@ using TMPro;
 public class Counter : MonoBehaviour
 {
     public static TextMeshProUGUI countText; // Reference to the TextMeshPro text component
+    public static TextMeshProUGUI scoreText;
     public static int count = 0;
 
     void Start()
     {
         countText = GameObject.Find("Text GameScore").GetComponent<TextMeshProUGUI>();
+        countText.text = "Score: " + count.ToString();// Set the initial text to "Score: 0"
 
-        // Set the initial text to "Score: 0"
-        countText.text = "Score: " + count.ToString();
+        scoreText = GameObject.Find("Text Score").GetComponent<TextMeshProUGUI>();
+        scoreText.text = "Result: " + count.ToString() + " kills";
+
     }
 
     // Function to increment the count by 1
@@ -19,11 +22,15 @@ public class Counter : MonoBehaviour
     {
         count++;
         countText.text = "Score: " + count.ToString();
+        scoreText.text = "Result: " + count.ToString() + " kills";
+
 
         // Check if count reaches 100
         if (count >= 100)
         {
-            Debug.Log("Congratulations! Count reached 100.");
+            //pt win
+            
+
         }
     }
 
